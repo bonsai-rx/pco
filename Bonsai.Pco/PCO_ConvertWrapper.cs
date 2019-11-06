d7e32e46cdaf4274d1694310bff04312a01f4454
glopes <glopesdev@users.noreply.github.com>
2019-11-06 13:29:53 +0000

    Standardize pinvoke wrapper calls.

--- Bonsai.Pco/PCO_ConvertWrapper.cs
+++ Bonsai.Pco/PCO_ConvertWrapper.cs
@@ -7,6 +7,8 @@ namespace Bonsai.Pco
 {
     static class PCO_Convert_LibWrapper
     {
+        const string LibName = "PCO_Conv";
+
         #region Class Members
         public const int PCO_BW_CONVERT = 0;
         public const int PCO_COLOR_CONVERT = 2;
@@ -25,34 +27,30 @@ namespace Bonsai.Pco
         // pSensorInfo = Pass by Ref SensorInfo structure
         // iConvertType = Int variable to determine the conversion type
         // returns Int error code value
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertCreate",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertCreate(ref IntPtr pHandle, ref PCO_ConvertStructures.PCO_SensorInfo pSensorInfo, int iConvertType);
 
         // Deletes a previously created convert object. Mandatory to call this before closing application
         // pHandle = Int handle to a previously created convert object
         // returns Int error code value
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertDelete",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertDelete(IntPtr pHandle);
 
         // Gets all the values of a previously created convert object
         // pHandle = Int handle to a previously created object
         // pConvert = Pass by Ref Convert structure
         // returns Int error code value
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertGet", ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertGet(IntPtr pHandle, ref PCO_ConvertStructures.PCO_Convert pConvert);
 
         #endregion
 
 
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertGetDisplay",
-                    ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertGetDisplay(IntPtr pHandle, ref PCO_ConvertStructures.PCO_Display pDisplay);
 
 
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertSetDisplay",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertSetDisplay(IntPtr pHandle, ref PCO_ConvertStructures.PCO_Display pDisplay);
 
         // Converts the camera raw 16 bit data to 8 bit b/w format
@@ -64,27 +62,23 @@ namespace Bonsai.Pco
         // rawImagePointer = Pointer to the raw image
         // resultingImagePointer = Pointer to allocated memory to store the resulting image
         // returns Int error code value
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_Convert16TO8",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_Convert16TO8(IntPtr pHandle, int imode, int icolormode, int width, int height, UIntPtr rawImagePointer, byte[] resultingImagePointer);
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_Convert16TOCOL",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_Convert16TOCOL(IntPtr pHandle, int imode, int icolormode, int width, int height, UIntPtr rawImagePointer, byte[] resultingImagePointer);
 
         // Sets the PCO_SensorInfo structure for a previously created convert object
         // pHandle = Handle to a previously created convert object
         // pSensorInfo = Pass by Ref Sensor information structure. Do not forget to set pSensorInfo.wSize
         // returns Int error code value
-        [DllImport("PCO_Conv.dll", EntryPoint = "PCO_ConvertSetSensorInfo",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_ConvertSetSensorInfo(IntPtr pHandle, ref PCO_ConvertStructures.PCO_SensorInfo pSensorInfo);
 
-        [DllImport("PCO_CDlg.dll", EntryPoint = "PCO_OpenConvertDialog",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_OpenConvertDialog(ref IntPtr hLutDialog, IntPtr parent, [MarshalAs(UnmanagedType.LPStr)]string title, int msg_id, IntPtr hlut, int xpos, int ypos);
 
-        [DllImport("PCO_CDlg.dll", EntryPoint = "PCO_CloseConvertDialog",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_CloseConvertDialog(IntPtr hLutDialog);
 
         // Sets the converted and raw image data to the convert dialog. This will update the histogram diagrams shown in the dialog
@@ -94,16 +88,13 @@ namespace Bonsai.Pco
         // b16_image = Pointer to the raw data
         // rgb_image = Pointer to the converted data
         // returns Int error code value
-        [DllImport("PCO_CDlg.dll", EntryPoint = "PCO_SetDataToDialog",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_SetDataToDialog(IntPtr hLutDialog, int ixres, int iyres, UIntPtr b16Image, byte[] rgbImage);
 
-        [DllImport("PCO_CDlg.dll", EntryPoint = "PCO_SetConvertDialog",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_SetConvertDialog(IntPtr hLutDialog, IntPtr hLut);
 
-        [DllImport("PCO_CDlg.dll", EntryPoint = "PCO_GetConvertDialog",
-            ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(LibName, CallingConvention = CallingConvention.StdCall)]
         public static extern int PCO_GetConvertDialog(IntPtr hLutDialog, IntPtr hLut);
     }
 }
