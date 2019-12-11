052f47935a543d6a4ee768fdf23ae263f31f787c
glopes <glopesdev@users.noreply.github.com>
2019-12-11 16:15:27 +0000

    Added support for reporting image timestamp and counter metadata.

---
+++ Bonsai.Pco/PcoDataFrame.cs
@@ -0,0 +1,16 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Pco
+{
+    public struct PcoDataFrame
+    {
+        public int Counter;
+        public DateTimeOffset Timestamp;
+        public IplImage Image;
+    }
+}
